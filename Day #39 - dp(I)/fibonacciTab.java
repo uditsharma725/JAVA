6aa3c0a177b52fd617c300b6611ca502119bc02d
uditsharma725 <72641703+uditsharma725@users.noreply.github.com>
2023-03-06 08:23:19 +0530

    minor changes in fibonacci tabulation code

--- Day #39 - dp(I)/fibonacciTab.java
+++ Day #39 - dp(I)/fibonacciTab.java
@@ -4,12 +4,10 @@ public class fibonacciTab {
 
     public static int fib(int n) {
         int dp[] = new int[n+1];
+        
         dp[0] = 0;
-        dp[1] = 1;
-
-        for(int i=2; i<=n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
+        for(int i=1; i<=n; i++)
+        dp[i] = dp[i-1] + (i == 1 ? 1 : dp[i-2]);
 
         return dp[n];
     }
@@ -19,7 +17,7 @@ public class fibonacciTab {
         Scanner sc = new Scanner(System.in);
         try {
 
-            int n = 25;
+            int n = 8;
             System.out.print(fib(n));
 
         } finally {
