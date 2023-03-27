79c310d1ce85ba5cee8c235e0bb2d7769aa64219
uditsharma725 <72641703+uditsharma725@users.noreply.github.com>
2023-03-27 11:09:09 +0530

    fixed errors in checkPalindrome code

--- Day #21 - linklist(I)/checkPalindrome.java
+++ Day #21 - linklist(I)/checkPalindrome.java
@@ -46,9 +46,9 @@ public class checkPalindrome {
         Node slow = head;
         Node fast = head;
 
-        while(fast!=null && fast.next!=null) {
+        while(fast.next!=null && fast.next.next!=null) {
             slow = slow.next;
-            fast = fast.next;
+            fast = fast.next.next;
         }
 
         return slow;
